b3c468def56fa6e4f707315baf2bd3d10919e40f
shiva@hotstar.com <shiva@hotstar.com>
2016-12-15 16:52:16 +0530

    Add configurations and script for deployment: Save the data locally from Hive
    Task-Url:

--- src/main/java/com/hotstar/datamodel/streaming/spark/iterator/IRecordIterator.java
+++
@@ -1,8 +0,0 @@
-package com.hotstar.datamodel.streaming.spark.iterator;
-
-import java.util.Iterator;
-
-public interface IRecordIterator extends Iterator<Object[]>{
-	public void init(byte[] data);
-	public void init(byte[] data, int offset, String pathString);
-}
